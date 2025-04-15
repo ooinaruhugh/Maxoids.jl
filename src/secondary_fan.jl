4e51661baa3fb5b912aa99de87036ed5d8d04c96
Kamillo Ferry <kafe@kafe.dev>
2025-04-15 21:58:11 +0200

    weights_from_cones, now with lower-dimensional cones

--- src/secondary_fan.jl
+++ src/secondary_fan.jl
@@ -1,10 +1,10 @@
 module OscarInterop
 
 using LinearAlgebra
-import Graphs as gr
 using Oscar
 using Polymake
 
+import Graphs as gr
 import Graphs: SimpleDiGraph
 import Oscar: Graph, Directed
 
@@ -25,7 +25,7 @@ function fundamental_polytope(::Type{Matrix}, G::Graph, R=ZZ)
   return A[1:m+1,n+1:end] - A[1:m+1,1:n]
 end
 
-function weights_for_cones(G::Graph{Directed})
+function weights_for_cones(G::Graph{Directed}; with_lower_dimensional=false)
   TT = tropical_semiring(max)
   A = [ones(ZZRingElem, n_edges(G)+1) fundamental_polytope(Matrix, G)]
 
@@ -34,18 +34,31 @@ function weights_for_cones(G::Graph{Directed})
 
   maximal_cones(sfan)
 
-  map(maximal_cones(sfan)) do C
-    v = rays_modulo_lineality(C) |> first |> sum
-    W = identity_matrix(TT, n_vertices(G))
-    for (e,w) in zip(edges(G), v[1:end-1])
-      W[src(e), dst(e)] = w
-    end
+  if with_lower_dimensional
+    return map(eachrow(cones(sfan))) do i
+      v = (rays_modulo_lineality(sfan) |> first)[i] |> sum
+      W = identity_matrix(TT, n_vertices(G))
 
-    W
+      for (e,w) in zip(edges(G), v[1:end-1])
+        W[src(e), dst(e)] = w
+      end
+
+      W
+    end
+  else
+    return map(maximal_cones(sfan)) do C
+      v = rays_modulo_lineality(C) |> first |> sum
+      W = identity_matrix(TT, n_vertices(G))
+      for (e,w) in zip(edges(G), v[1:end-1])
+        W[src(e), dst(e)] = w
+      end
+
+      W
+    end
   end
 end
 
-function weights_for_cones(H::SimpleDiGraph)
+function weights_for_cones(H::SimpleDiGraph; with_lower_dimensional=false)
   n = gr.nv(H)
   G = Graph{Directed}(n)
 
@@ -53,7 +66,7 @@ function weights_for_cones(H::SimpleDiGraph)
     add_edge!(G, gr.src(e), gr.dst(e))
   end
 
-  return weights_for_cones(G)
+  return weights_for_cones(G; with_lower_dimensional=with_lower_dimensional)
 end
 
 export weights_for_cones
