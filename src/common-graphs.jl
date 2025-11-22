06d635a40b3759586d0c139b26d261ac54367f68
Kamillo Ferry <kafe@kafe.dev>
2025-11-23 00:31:07 +0100

    Change the predefined graphs to polymake

--- src/common-graphs.jl
+++ src/common-graphs.jl
@@ -6,14 +6,14 @@ function complete_DAG(d)
   return graph_from_adjacency_matrix(Directed,E)
 end
 
-const complete_3DAG = complete_DAG(3)
-const collider = graph_from_edges(Directed, [[1,3],[2,3]])
-const three_path = graph_from_edges(Directed, [[1,2],[2,3]])
+complete_3DAG() = complete_DAG(3)
+collider() = graph_from_edges(Directed, [[1,3],[2,3]])
+three_path() = graph_from_edges(Directed, [[1,2],[2,3]])
 
-const diamond = graph_from_edges(Directed, [[1,2],[1,3],[2,4],[3,4]])
-const diamond_var = graph_from_edges(Directed, [[1,4],[1,2],[2,3],[3,4]])
-const diamond_double_collider = graph_from_edges(Directed, [[1,3],[2,3],[1,4],[2,4]])
-const diamond_with_14 = graph_from_edges(Directed, [[1,2],[1,3],[2,4],[3,4],[1,4]])
-const complete_4DAG = complete_DAG(4)
+diamond() = graph_from_edges(Directed, [[1,2],[1,3],[2,4],[3,4]])
+diamond_var() = graph_from_edges(Directed, [[1,4],[1,2],[2,3],[3,4]])
+diamond_double_collider() = graph_from_edges(Directed, [[1,3],[2,3],[1,4],[2,4]])
+diamond_with_14() = graph_from_edges(Directed, [[1,2],[1,3],[2,4],[3,4],[1,4]])
+complete_4DAG() = complete_DAG(4)
 
-const cassiopeia = graph_from_edges(Directed, [[1,4],[2,4],[2,5],[3,5]])
+cassiopeia() = graph_from_edges(Directed, [[1,4],[2,4],[2,5],[3,5]])
