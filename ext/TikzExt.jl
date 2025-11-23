51b9ec68ac7c26953d19820712a00ba38f0617c0
Kamillo Ferry <kafe@kafe.dev>
2025-11-23 19:54:58 +0100

    Clean up tikz ext code

--- ext/TikzExt.jl
+++ ext/TikzExt.jl
@@ -1,15 +1,15 @@
 module TikzExt
 
 using Oscar,Maxoids
-using TikzPictures,TikzGraphs,Serialization
+using TikzPictures,TikzGraphs
 
-function DAG_to_pdf(G::Graph{Directed}, name::String)
+function Maxoids.DAG_to_pdf(G::Graph{Directed}, name::String)
   _G = Maxoids.to_graphs_graph(G)
   t = TikzGraphs.plot(_G)
   TikzGraphs.save(PDF(name* ".pdf"), t)
 end
 
-function graph_to_pdf(G::Graph{Undirected}, name::String)
+function Maxoids.graph_to_pdf(G::Graph{Undirected}, name::String)
   _G = Maxoids.to_graphs_graph(G)
   t = TikzGraphs.plot(_G)
   TikzGraphs.save(PDF(name* ".pdf"), t)
