df8721f7eb30d72ddf964c4b91d5f12b22f81508
Francesco Nowell <francesconowell@gmail.com>
2025-11-26 12:35:20 +0100

    changed example 2.8 to be consistent with paper

--- examples/example2-8.jl
+++ examples/example2-8.jl
@@ -1,20 +1,21 @@
-using Oscar, Maxoids
+using  Maxoids, Oscar;
 
-E = [[1,2],[1,3],[2,3],[2,4],[3,4]]
-G = graph_from_edges(Directed, E)
+E = [[1,2],[1,3],[2,3],[2,4],[3,4]];
+G = graph_from_edges(Directed, E);
 
-C = weights_to_tropical_matrix(G, [1,1,1,3,1])
+C = weights_to_tropical_matrix(G, [1,1,1,3,1]);
 
 G_tr, C_tr = weighted_transitive_reduction(G,C)
 
-Gstar = transitive_closure(G)
-Cstar = kleene_star(C)
+Gbar = transitive_closure(G)
+Cbar = weights_to_tropical_matrix(Gbar, [1,1,1,1,3,1])
 
 M     = maxoid(G,C)
 M_tr  = maxoid(G_tr,C_tr)
-Mstar = maxoid(Gstar,Cstar)
+Mbar = maxoid(Gbar,Cbar)
 
+M == M_tr == Mbar 
 println("M*(G,C)       = $(M)")
 println("M*(G_tr,C_tr) = $(M_tr)")
-println("M*(G*,C*)     = $(Mstar)")
-println(M == M_tr && M_tr == Mstar)
+println("M*(G*,C*)     = $(Mbar)")
+println(M == M_tr && M_tr == Mbar)
